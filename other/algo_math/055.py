0fb6fa8d5e44521224db2b35383798839abb6d4e
aruaru0 <aruaru0@gmail.com>
2023-09-16 09:36:52 +0900

    algo_math-055

---
+++ other/algo_math/055.py
@@ -0,0 +1,34 @@
+def matmul(A, B, mod):
+    N = len(A)
+    K = len(A[0])
+    M = len(B[0])
+
+    c = [[0 for _ in range(M)] for _ in range(N)]
+
+    for i in range(N) :
+        for j in range(K) :
+            for k in range(M) :
+                c[i][k] += A[i][j] * B[j][k] 
+                c[i][k] %= mod
+    return c
+
+def pow_matrix(A, p, mod):
+    n = len(A)
+    c = [[1 if i == j else 0 for i in range(n)] for j in range(n)]
+    while p > 0 :
+        if p%2 == 1 :
+            c = matmul(c, A, mod)
+        A = matmul(A, A, mod)
+        p //= 2
+    return c
+
+
+n = int(input())
+
+a = [[2,1],[1,0]]
+
+mod = 10**9+7
+ret = pow_matrix(a, n-2, mod)
+
+ans = sum(ret[0]) % mod
+print(ans)
