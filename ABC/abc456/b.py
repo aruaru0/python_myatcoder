cd507d861ec4d1a70617f0c486a592f5db6e9b8b
aruaru0 <aruaru0@gmail.com>
2026-05-03 09:47:29 +0900

    abc456-B

---
+++ ABC/abc456/b.py
@@ -0,0 +1,12 @@
+dice = []
+for _ in range(3):
+    dice.append(list(map(int, input().split())))
+
+cnt = 0
+for a in dice[0]:
+    for b in dice[1]:
+        for c in dice[2]:
+            if sorted([a, b, c]) == [4, 5, 6]:
+                cnt += 1
+
+print(cnt / 216)
